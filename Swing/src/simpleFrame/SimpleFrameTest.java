ef552bc514a5f07fae0fd91bc684bc86217ad0de
IrinaPeresunko <peresunkoi@gmail.com>
2015-05-04 23:09:46 +0300

    simple frame test

---
+++ Swing/src/simpleFrame/SimpleFrameTest.java
@@ -0,0 +1,30 @@
+package simpleFrame;
+import java.awt.*;
+import javax.swing.*;
+
+public class SimpleFrameTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(new Runnable()
+         {
+            public void run()
+            {
+               SimpleFrame frame = new SimpleFrame();
+               frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+               frame.setVisible(true);
+            }
+         });
+   }
+}
+
+class SimpleFrame extends JFrame
+{
+   public SimpleFrame()
+   {
+      setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+   }
+
+   public static final int DEFAULT_WIDTH = 300;
+   public static final int DEFAULT_HEIGHT = 200;
+}
